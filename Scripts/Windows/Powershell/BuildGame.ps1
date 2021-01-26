8f6df65da969878614c6ae2416f73d111ac55a96
Mikael Kalms <mikael@kalms.org>
2021-01-26 08:10:09 +0100

    param() should be at start of file

--- Scripts/Windows/Powershell/BuildGame.ps1
+++ Scripts/Windows/Powershell/BuildGame.ps1
@@ -1,6 +1,3 @@
-
-. $PSScriptRoot\Run-UAT.ps1
-
 param (
 	[Parameter(Mandatory)] [string] $ProjectLocation,
 	[Parameter(Mandatory)] [string] $TargetPlatform,
@@ -8,5 +5,7 @@ param (
 	[Parameter(Mandatory)] [string] $Target,
 	[Parameter(Mandatory)] [string] $ArchiveDir
 )
+
+. $PSScriptRoot\Run-UAT.ps1
 
 Run-UAT -UProjectLocation $ProjectLocation -Arguments "-ScriptsForProject=$(Resolve-Path $ProjectLocation)", "BuildCookRun", "-installed", "-nop4", "-project=$(Resolve-Path $ProjectLocation)", "-cook", "-stage", "-archive", "-archivedirectory=$(Resolve-Path $ArchiveDir)", "-package", "-pak", "-prereqs", "-nodebuginfo", "-targetplatform=$TargetPlatform", "-build", "-target=$Target", "-clientconfig=$Configuration", "-utf8output", "-buildmachine", "-iterativecooking", "-iterativedeploy", "-NoCodeSign"
