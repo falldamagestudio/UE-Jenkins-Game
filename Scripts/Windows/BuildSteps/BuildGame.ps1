c155b92e01d139bd13a03f95bcfd636ce763049c
Mikael Kalms <mikael@kalms.org>
2021-05-01 14:43:39 +0200

    Create archive folder if it doesn't exist

--- Scripts/Windows/BuildSteps/BuildGame.ps1
+++ Scripts/Windows/BuildSteps/BuildGame.ps1
@@ -8,4 +8,7 @@ param (
 
 . $PSScriptRoot\..\Powershell\Run-UAT.ps1
 
+# Create archive folder if it does not already exist
+New-Item -Path $ArchiveDir -ItemType Directory -Force -ErrorAction Stop | Out-Null
+
 Run-UAT -UProjectLocation $ProjectLocation -Arguments "-ScriptsForProject=$(Resolve-Path $ProjectLocation)", "BuildCookRun", "-installed", "-nop4", "-project=$(Resolve-Path $ProjectLocation)", "-cook", "-stage", "-archive", "-archivedirectory=$(Resolve-Path $ArchiveDir)", "-package", "-pak", "-prereqs", "-nodebuginfo", "-targetplatform=$TargetPlatform", "-build", "-target=$Target", "-clientconfig=$Configuration", "-utf8output", "-buildmachine", "-iterativecooking", "-iterativedeploy", "-NoCodeSign"
