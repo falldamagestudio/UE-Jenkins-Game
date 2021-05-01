c6567cbba9f5fc959c5b125fcdf5284d23a62501
Mikael Kalms <mikael@kalms.org>
2021-05-01 14:28:03 +0200

    Shuffled blocks again + better comment

--- build_game_win64_git_docker.groovy
+++ build_game_win64_git_docker.groovy
@@ -26,22 +26,27 @@ pipeline {
 
     stage('Update UE') {
 
-      // Any programs that use the GCP Client Library (for example, Longtail) will by default use
-      //   the provided service account
-      // This makes behaviour identical between running on a GCE VM and a bare-metal host
-      //
-      // Besides, the GCP Client Library attempts to access the internal endpoint (169.254.169.254) to 
-      //   detect the VM identity -- and if run within a Docker container on a GCE VM, traffic to that location
-      //   does not work properly; the net result is that a program such as Longtail will hang indefinitely
-      //   if invoked within a Docker container on a GCE VM if GOOGLE_APPLICATION_CREDENTIALS is not set
-      //
-      // Reference: https://github.com/jenkinsci/google-oauth-plugin/issues/6#issuecomment-431424049
-      // Reference: https://cloud.google.com/docs/authentication/production#automatically
-      // Reference: https://jenkinsci.github.io/kubernetes-credentials-provider-plugin/examples/
+      steps {
 
-      withCredentials([[$class: 'FileBinding', credentialsId: 'build-job-gcp-service-account-key', variable: 'GOOGLE_APPLICATION_CREDENTIALS']]) {
+        // Ensure that any program that uses the GCP Client Library (for example, Longtail) uses
+        //   the provide service account as its identity.
+        //
+        // This makes the application autheticate identically in different environments,
+        //   regardless of whether it's run on a bare-metal host, a GCE VM, or in a GKE Pod,
+        //   regardless of whether it's run within a Docker container or natively.
+        //
+        // In addition, the GCP Client Library attempts to access the internal endpoint (169.254.169.254) to 
+        //   figure out if it's run on GCE/GKE and then pick its identity -- and if run within a Docker
+        //   container on a GCE VM, traffic to that endpoint does not get routed properly by default; the net
+        //   result is that a program such as Longtail will hang indefinitely if invoked within a Docker
+        //   container on a GCE VM where GOOGLE_APPLICATION_CREDENTIALS is not set.
+        //
+        // Reference: https://github.com/jenkinsci/google-oauth-plugin/issues/6#issuecomment-431424049
+        // Reference: https://cloud.google.com/docs/authentication/production#automatically
+        // Reference: https://jenkinsci.github.io/kubernetes-credentials-provider-plugin/examples/
 
-        steps {
+        withCredentials([[$class: 'FileBinding', credentialsId: 'build-job-gcp-service-account-key', variable: 'GOOGLE_APPLICATION_CREDENTIALS']]) {
+
           powershell """
             try {
               & .\\Scripts\\Windows\\BuildSteps\\UpdateUE.ps1 -CloudStorageBucket ${LONGTAIL_STORE_BUCKET_NAME}
