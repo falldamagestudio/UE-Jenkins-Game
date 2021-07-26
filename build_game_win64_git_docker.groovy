336c610b0f8d3da0c3f28a6f07a7ae05f25845b0
Mikael Kalms <mikael@kalms.org>
2021-07-26 14:00:06 +0200

    Removed need for forced entrypoint for windows containers
    The latest itetation of buildtools containers have CMD/ENTRYPOINT set the way that Jenkins expects them

--- build_game_win64_git_docker.groovy
+++ build_game_win64_git_docker.groovy
@@ -8,13 +8,6 @@ pipeline {
       // All steps will be performed within this container
       image env.UE_JENKINS_BUILDTOOLS_WINDOWS_IMAGE
 
-      // Ensure that cmd.exe is running as soon as the container has started.
-      // Without this, if the container default to, for example, powershell, then Docker will report the following error:
-      //  ERROR: The container started but didn't run the expected command. Please double check your ENTRYPOINT does execute the command passed as docker run argument, as required by official docker images (see https://github.com/docker-library/official-images#consistency for entrypoint consistency requirements).
-      //  Alternatively you can force image entrypoint to be disabled by adding option `--entrypoint=''`.
-      // The error is benign (the job will continue and will work succrssfully), but confusing.
-      args '--entrypoint=cmd.exe'
-
       // Use a specific workspace folder, with a shorter folder name (Jenkins will default to C:\J\workspace\build_engine_windows_docker).
       // Building UE results in some long paths, and paths longer than 260 characters are problematic under Windows.
       // This shorter custom workspace name minimizes the risk that we'll run into too-long path names.
